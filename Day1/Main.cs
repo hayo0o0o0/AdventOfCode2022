9ceaf2626d288c229f5c8bcda2db6d0ee975c0e8
Hayo <6409301+hayo0o0o0@users.noreply.github.com>
2022-12-29 11:07:26 +0100

    Initial commit

---
+++ Day1/Main.cs
@@ -0,0 +1,31 @@
+﻿namespace Day1;
+
+public class Main
+{
+    public static void Part1()
+    {
+        var calories = ReadCalories();
+        var maxCalories = calories.Max();
+
+        Console.WriteLine("Highest calories are {0}", maxCalories);
+    }
+
+    public static void Part2()
+    {
+        var calories = ReadCalories();
+        var top3 = calories.OrderByDescending(x=>x).Take(3);
+        var totalCalories = top3.Sum();
+        
+        Console.WriteLine("The top 3 elves are carrying {0} calories", totalCalories);
+    }
+
+    private static IEnumerable<int> ReadCalories()
+    {
+        var fileData = File.ReadAllText("input.txt");
+        var elves = fileData.Split($"{Environment.NewLine}{Environment.NewLine}",
+            StringSplitOptions.RemoveEmptyEntries);
+        var calories = elves.Select(x =>
+            x.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).Sum());
+        return calories;
+    }
+}
