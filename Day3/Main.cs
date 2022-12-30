becf4f21a15be4c3ce2a40eeb50e2be34f620098
Hayo <6409301+hayo0o0o0@users.noreply.github.com>
2022-12-30 13:12:00 +0100

    Day3/Day4

---
+++ Day3/Main.cs
@@ -0,0 +1,48 @@
+﻿namespace Day3;
+
+public static class Main
+{
+    public static void Part1()
+    {
+        var fileData = File.ReadAllLines("input.txt");
+        var totalPriorities = 0;
+        foreach (var backpack in fileData)
+        {
+            var backpackLength = backpack.Length / 2;
+
+            var firstCompartment = backpack[..backpackLength];
+            var secondCompartment = backpack[backpackLength..];
+            var duplicateItems = firstCompartment.Intersect(secondCompartment);
+            totalPriorities += duplicateItems.Sum(duplicateItem => duplicateItem.GetNumber());
+        }
+
+        Console.WriteLine("Sum of the priorities is: {0}", totalPriorities);
+    }
+
+    public static void Part2()
+    {
+        var fileData = File.ReadAllLines("input.txt");
+        var totalPriorities = 0;
+
+        for (var i = 0; i < fileData.Length; i += 3)
+        {
+            var group = fileData.Skip(i).Take(3);
+            var elf1 = group.ElementAt(0);
+            var elf2 = group.ElementAt(1);
+            var elf3 = group.ElementAt(2);
+            var duplicateItems = elf1.Intersect(elf2);
+            duplicateItems = duplicateItems.Intersect(elf3);
+            totalPriorities += duplicateItems.Sum(duplicateItem => duplicateItem.GetNumber());
+        }
+
+        Console.WriteLine("Sum of the priorities is: {0}", totalPriorities);
+    }
+
+
+    private static int GetNumber(this char character)
+    {
+        if (char.IsUpper(character)) return character - 'A' + 27;
+
+        return character - 'a' + 1;
+    }
+}
---
+++ Day4/Main.cs
@@ -0,0 +1,81 @@
+﻿namespace Day4;
+
+public class Main
+{
+    public static void Part1()
+    {
+        var fileData = File.ReadAllLines("input.txt");
+        var amountOfPairsForReconsideration = 0;
+        foreach (var cleaningPair in fileData)
+        {
+            var splitPairs = cleaningPair.Split(",");
+            var firstAssignment = GetRoomsToClean(splitPairs[0]);
+            var secondAssignment = GetRoomsToClean(splitPairs[1]);
+
+            if (FullyOverlap(secondAssignment, firstAssignment))
+            {
+                amountOfPairsForReconsideration++;
+            }
+            else if (FullyOverlap(firstAssignment, secondAssignment))
+            {
+                amountOfPairsForReconsideration++;
+            }
+        }
+
+        Console.WriteLine("Amount of pairs that need reconsideration: {0}", amountOfPairsForReconsideration);
+    }
+
+    public static void Part2()
+    {
+        var fileData = File.ReadAllLines("input.txt");
+        var amountOfPairsForReconsideration = 0;
+        foreach (var cleaningPair in fileData)
+        {
+            var splitPairs = cleaningPair.Split(",");
+            var firstAssignment = GetRoomsToClean(splitPairs[0]);
+            var secondAssignment = GetRoomsToClean(splitPairs[1]);
+
+            if (PartialOverlap(secondAssignment, firstAssignment))
+            {
+                amountOfPairsForReconsideration++;
+            }
+            else if (PartialOverlap(firstAssignment, secondAssignment))
+            {
+                amountOfPairsForReconsideration++;
+            }
+        }
+
+        Console.WriteLine("Amount of pairs that need reconsideration: {0}", amountOfPairsForReconsideration);
+    }
+
+    private static bool FullyOverlap(IEnumerable<int> range1, IEnumerable<int> range2)
+    {
+        var remaining = range1.Except(range2);
+        return !remaining.Any();
+    }
+    
+    private static bool PartialOverlap(IEnumerable<int> range1, IEnumerable<int> range2)
+    {
+        var remaining = range1.Intersect(range2);
+        return remaining.Any();
+    }
+
+    private static IEnumerable<int> GetRoomsToClean(string assignment)
+    {
+        var roomNumbers = assignment.Split('-').Select(int.Parse).ToArray();
+        return GetRange(roomNumbers[0], roomNumbers[1]);
+    }
+
+    private static IEnumerable<int> GetRange(int from, int to)
+    {
+        if (from == to)
+        {
+            yield return from;
+        }
+
+        for (var i = from; i <= to; i++)
+        {
+            yield return i;
+        }
+    }
+}
