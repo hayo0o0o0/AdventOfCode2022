8e85f738e170e6b23b03bc9b7a6e01c1b1702603
Hayo <6409301+hayo0o0o0@users.noreply.github.com>
2023-01-01 16:44:33 +0100

    Day 6 part 2

--- Day6/Main.cs
+++ Day6/Main.cs
@@ -6,13 +6,29 @@ public static class Main
     {
         var input = File.ReadAllText("input.txt");
         var start = 0;
-        var marker = "    ";
+        var marker = "";
         var end = 4;
         while (marker.Distinct().Count() != 4)
         {
             marker = input[start..end];
             start++;
             end = start + 4;
+        }
+
+        Console.WriteLine("Marker: {1} Answer: {0}", end-1, marker);
+    }
+    
+    public static void Part2()
+    {
+        var input = File.ReadAllText("input.txt");
+        var start = 0;
+        var marker = "";
+        var end = 14;
+        while (marker.Distinct().Count() != 14)
+        {
+            marker = input[start..end];
+            start++;
+            end = start + 14;
         }
 
         Console.WriteLine("Marker: {1} Answer: {0}", end-1, marker);
