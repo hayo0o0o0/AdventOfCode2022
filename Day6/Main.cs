9da6acc0ce5a59aa230a1ab92058a4011c79ca48
Hayo <6409301+hayo0o0o0@users.noreply.github.com>
2023-01-01 16:10:31 +0100

    Day6 part1

---
+++ Day6/Main.cs
@@ -0,0 +1,20 @@
+﻿namespace Day6;
+
+public static class Main
+{
+    public static void Part1()
+    {
+        var input = File.ReadAllText("input.txt");
+        var start = 0;
+        var marker = "    ";
+        var end = 4;
+        while (marker.Distinct().Count() != 4)
+        {
+            marker = input[start..end];
+            start++;
+            end = start + 4;
+        }
+
+        Console.WriteLine("Marker: {1} Answer: {0}", end-1, marker);
+    }
+}
